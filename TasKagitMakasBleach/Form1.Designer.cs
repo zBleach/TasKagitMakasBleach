ebfe2f2f817e8bfe88ea09e4f864631b9788d3db
zBleach <79703662+zBleach@users.noreply.github.com>
2021-02-26 16:20:15 +0300

    Initial commit

---
+++ TasKagitMakasBleach/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace TasKagitMakasBleach
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tasbtn = new System.Windows.Forms.Button();
+            this.kagitbtn = new System.Windows.Forms.Button();
+            this.makasbtn = new System.Windows.Forms.Button();
+            this.lblkullaniciad = new System.Windows.Forms.Label();
+            this.p1secim = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.botsecim = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.p1sonuc = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.botsonuc = new System.Windows.Forms.Label();
+            this.sonuclbl = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tasbtn
+            // 
+            this.tasbtn.Font = new System.Drawing.Font("Microsoft JhengHei", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tasbtn.Location = new System.Drawing.Point(233, 426);
+            this.tasbtn.Name = "tasbtn";
+            this.tasbtn.Size = new System.Drawing.Size(152, 69);
+            this.tasbtn.TabIndex = 0;
+            this.tasbtn.Text = "Taş";
+            this.tasbtn.UseVisualStyleBackColor = true;
+            this.tasbtn.Click += new System.EventHandler(this.tasbtn_Click);
+            // 
+            // kagitbtn
+            // 
+            this.kagitbtn.Font = new System.Drawing.Font("Microsoft JhengHei", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.kagitbtn.Location = new System.Drawing.Point(422, 426);
+            this.kagitbtn.Name = "kagitbtn";
+            this.kagitbtn.Size = new System.Drawing.Size(152, 69);
+            this.kagitbtn.TabIndex = 1;
+            this.kagitbtn.Text = "Kağıt";
+            this.kagitbtn.UseVisualStyleBackColor = true;
+            this.kagitbtn.Click += new System.EventHandler(this.kagitbtn_Click);
+            // 
+            // makasbtn
+            // 
+            this.makasbtn.Font = new System.Drawing.Font("Microsoft JhengHei", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.makasbtn.Location = new System.Drawing.Point(611, 426);
+            this.makasbtn.Name = "makasbtn";
+            this.makasbtn.Size = new System.Drawing.Size(152, 69);
+            this.makasbtn.TabIndex = 2;
+            this.makasbtn.Text = "Makas";
+            this.makasbtn.UseVisualStyleBackColor = true;
+            this.makasbtn.Click += new System.EventHandler(this.makasbtn_Click);
+            // 
+            // lblkullaniciad
+            // 
+            this.lblkullaniciad.AutoSize = true;
+            this.lblkullaniciad.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblkullaniciad.Location = new System.Drawing.Point(12, 105);
+            this.lblkullaniciad.Name = "lblkullaniciad";
+            this.lblkullaniciad.Size = new System.Drawing.Size(115, 45);
+            this.lblkullaniciad.TabIndex = 3;
+            this.lblkullaniciad.Text = "Bekir : ";
+            // 
+            // p1secim
+            // 
+            this.p1secim.AutoSize = true;
+            this.p1secim.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.p1secim.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.p1secim.Location = new System.Drawing.Point(161, 115);
+            this.p1secim.Name = "p1secim";
+            this.p1secim.Size = new System.Drawing.Size(156, 32);
+            this.p1secim.TabIndex = 6;
+            this.p1secim.Text = "Bekleniyor..";
+            this.p1secim.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(690, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 45);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Bedi : ";
+            // 
+            // botsecim
+            // 
+            this.botsecim.AutoSize = true;
+            this.botsecim.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.botsecim.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.botsecim.Location = new System.Drawing.Point(778, 118);
+            this.botsecim.Name = "botsecim";
+            this.botsecim.Size = new System.Drawing.Size(156, 32);
+            this.botsecim.TabIndex = 8;
+            this.botsecim.Text = "Bekleniyor..";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.p1sonuc);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(288, 176);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 149);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "PLAYER 1";
+            // 
+            // p1sonuc
+            // 
+            this.p1sonuc.AutoSize = true;
+            this.p1sonuc.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.p1sonuc.Location = new System.Drawing.Point(59, 29);
+            this.p1sonuc.Name = "p1sonuc";
+            this.p1sonuc.Size = new System.Drawing.Size(74, 86);
+            this.p1sonuc.TabIndex = 0;
+            this.p1sonuc.Text = "0";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.botsonuc);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(494, 176);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 149);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "BOT";
+            // 
+            // botsonuc
+            // 
+            this.botsonuc.AutoSize = true;
+            this.botsonuc.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.botsonuc.Location = new System.Drawing.Point(65, 29);
+            this.botsonuc.Name = "botsonuc";
+            this.botsonuc.Size = new System.Drawing.Size(74, 86);
+            this.botsonuc.TabIndex = 0;
+            this.botsonuc.Text = "0";
+            // 
+            // sonuclbl
+            // 
+            this.sonuclbl.AutoSize = true;
+            this.sonuclbl.Font = new System.Drawing.Font("Segoe UI Black", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.sonuclbl.Location = new System.Drawing.Point(439, 23);
+            this.sonuclbl.Name = "sonuclbl";
+            this.sonuclbl.Size = new System.Drawing.Size(119, 40);
+            this.sonuclbl.TabIndex = 11;
+            this.sonuclbl.Text = "SONUC";
+            this.sonuclbl.Click += new System.EventHandler(this.sonuclbl_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(956, 542);
+            this.Controls.Add(this.sonuclbl);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.botsecim);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.p1secim);
+            this.Controls.Add(this.lblkullaniciad);
+            this.Controls.Add(this.makasbtn);
+            this.Controls.Add(this.kagitbtn);
+            this.Controls.Add(this.tasbtn);
+            this.ForeColor = System.Drawing.Color.Red;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tasbtn;
+        private System.Windows.Forms.Button kagitbtn;
+        private System.Windows.Forms.Button makasbtn;
+        private System.Windows.Forms.Label lblkullaniciad;
+        private System.Windows.Forms.Label p1secim;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label botsecim;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label p1sonuc;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label botsonuc;
+        private System.Windows.Forms.Label sonuclbl;
+    }
+}
+
---
+++ TasKagitMakasBleach/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TasKagitMakasBleach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int bot = 0;
+        int p1 = 0;
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tasbtn_Click(object sender, EventArgs e)
+        {
+            if (tasbtn.Enabled)
+            {
+                p1secim.Text = "Taş";
+                Pc();
+                Sonuc();
+            }
+        }
+
+        private void kagitbtn_Click(object sender, EventArgs e)
+        {
+            if (kagitbtn.Enabled)
+            {
+                p1secim.Text = "Kağıt";
+                Pc();
+                Sonuc();
+            }
+        }
+
+        private void makasbtn_Click(object sender, EventArgs e)
+        {
+            if (makasbtn.Enabled)
+            {
+                p1secim.Text = "Makas";
+                Pc();
+                Sonuc();
+            }
+        }
+
+
+        private void Pc()
+        {
+            Random pc = new Random();
+            int deger = pc.Next(0, 3);
+
+            if (deger == 0)
+            {
+                botsecim.Text = "Taş";
+            }
+            else if (deger == 1)
+            {
+                botsecim.Text = "Kağıt";
+            }
+            else
+            {
+                botsecim.Text = "Makas";
+            }
+        }
+
+        private void Sonuc()
+        {
+            if (p1secim.Text == botsecim.Text)
+            {
+                sonuclbl.Text = "Berabere";
+            }
+            if (p1secim.Text == "Taş" && botsecim.Text == "Kağıt") // Taşa Kağıt
+            {
+                sonuclbl.Text = "Kaybettin";
+                bot++;
+                botsonuc.Text = bot.ToString();
+            }
+            if (p1secim.Text == "Taş" && botsecim.Text == "Makas") // Taşa Makas
+            {
+                sonuclbl.Text = "Kazandın";
+                p1++;
+                p1sonuc.Text = p1.ToString();
+            }
+            if (p1secim.Text == "Makas" && botsecim.Text == "Kağıt") // Makasa Kağıt
+            {
+                sonuclbl.Text = "Kazandın";
+                p1++;
+                p1sonuc.Text = p1.ToString();
+            }
+            if (p1secim.Text == "Makas" && botsecim.Text == "Taş") // Makasa Taş
+            {
+                sonuclbl.Text = "Kaybettin";
+                bot++;
+                botsonuc.Text = bot.ToString();
+            }
+            if (p1secim.Text == "Kağıt" && botsecim.Text == "Makas") // Kağıta Makas
+            {
+                sonuclbl.Text = "Kaybettin";
+                bot++;
+                botsonuc.Text = bot.ToString();
+            }
+            if (p1secim.Text == "Kağıt" && botsecim.Text == "Taş") // Kağıta Taş
+            {
+                sonuclbl.Text = "Kazandın";
+                p1++;
+                p1sonuc.Text = p1.ToString();
+            }
+
+        }
+
+        private void sonuclbl_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
